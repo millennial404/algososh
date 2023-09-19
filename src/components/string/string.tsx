690aa310e59c9c2bf5e4ce61ccdc8f2e07f5ea88
Sergei Zakharchenko <millennial404@yandex.ru>
2023-09-20 00:40:14 +0300

    feat: string.tsx add form

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import styles from "./string.module.css";
+import {Circle} from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.wrapper}>
+        <div className={styles.form}>
+        <Input extraClass={'mr-6'} maxLength={11} isLimitText={true}/>
+        <Button text={'Развернуть'}/>
+        </div>
+        <div className={styles.vizualization}>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+        </div>
+      </div>
     </SolutionLayout>
+
   );
 };
