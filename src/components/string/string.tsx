78323c50b31762df11f1d1d4181b4291b631d964
Sergei Zakharchenko <millennial404@yandex.ru>
2023-11-14 00:02:19 +0300

    feat: string.tsx completed

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import {Input} from "../ui/input/input";
 import {Button} from "../ui/button/button";
@@ -7,51 +7,69 @@ import {Circle} from "../ui/circle/circle";
 import {useForm} from "../../hooks/useForm";
 import {ElementStates} from "../../types/element-states";
 
-export const swapper = (arr: string[]) => {
-    const newArr = [...arr];
-    if (newArr.length <= 1) {
-        return arr;
-    }
-    let leftCharIndex = 0;
-    let rightCharIndex = newArr.length - 1;
-    let leftChar = newArr[leftCharIndex];
-    let rightChar = newArr[rightCharIndex];
-    newArr[leftCharIndex] = rightChar;
-    newArr[rightCharIndex] = leftChar;
-    leftCharIndex++;
-    rightCharIndex--;
-
-    return newArr
+export const swapper = async (arr: string[], leftIndex: number, rightIndex: number) => {
+  const newArr = [...arr];
+  if (newArr.length <= 1) {
+    return arr;
+  }
+  let leftCharIndex = leftIndex;
+  let rightCharIndex = rightIndex;
+  let leftChar = newArr[leftCharIndex];
+  let rightChar = newArr[rightCharIndex];
+  newArr[leftCharIndex] = rightChar;
+  newArr[rightCharIndex] = leftChar;
+  return newArr
 }
 
 export const StringComponent: React.FC = () => {
-    const {values, handleChange,} = useForm({});
-    const [arrayChars, setArrayChars] = React.useState<string[]>([]);
-    const onSubmit = (evt: React.FormEvent) => {
-        evt.preventDefault();
-        const newChars = values.string?.split('') || [];
-        setArrayChars(newChars);
-        setTimeout(() => setArrayChars(swapper(newChars)), 1000);
-
+  const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
+  const {values, handleChange,} = useForm({});
+  const [arrayChars, setArrayChars] = useState<string[]>([]);
+  const [changingElements, setChangingElement] = useState<{ left: number | null, right: number | null }>({
+    left: null,
+    right: null
+  });
+  const [modifiedElements, setModifiedElement] = useState<number[]>([]);
+  const [completed, setCompleted] = useState(false);
+  const onSubmit = async (evt: React.FormEvent) => {
+    evt.preventDefault();
+    setCompleted(true);
+    let newChars = values.string?.split('') || [];
+    const modifiedElementsIndexes: number[] = [];
+    setModifiedElement([]);
+    setArrayChars(newChars);
+    setChangingElement({left: 0, right: newChars.length - 1});
+    for (let i = 0; i < newChars.length / 2; i++) {
+      setChangingElement({left: i, right: newChars.length - 1 - i});
+      await delay(1000);
+      const swappedChars = await swapper(newChars, i, newChars.length - 1 - i);
+      setArrayChars(swappedChars);
+      modifiedElementsIndexes.push(i, newChars.length - 1 - i);
+      setModifiedElement([...modifiedElementsIndexes]);
+      newChars = [...swappedChars];
     }
-
-    console.log(arrayChars, values.string)
-    const l = 0;
-    const r = 5
-    return (
-        <SolutionLayout title="Строка">
-            <div className={styles.wrapper}>
-                <form className={styles.form} onSubmit={onSubmit}>
-                    <Input name={'string'} extraClass={'mr-6'} maxLength={11} isLimitText={true}
-                           onChange={handleChange}/>
-                    <Button text={'Развернуть'} type={'submit'}/>
-                </form>
-                <ul className={styles.vizualization}>
-                    {arrayChars?.map((item: string, index: number) => <li key={index}><Circle
-                        state={l === index || r === index ? ElementStates.Changing : ElementStates.Default}
-                        letter={item}/></li>)}
-                </ul>
-            </div>
-        </SolutionLayout>
-    );
+    setCompleted(false);
+  }
+  console.log(modifiedElements)
+  return (
+    <SolutionLayout title="Строка">
+      <div className={styles.wrapper}>
+        <form className={styles.form} onSubmit={onSubmit}>
+          <Input name={'string'} extraClass={'mr-6'} maxLength={11} isLimitText={true}
+                 onChange={handleChange}/>
+          <Button disabled={completed} text={'Развернуть'} type={'submit'}/>
+        </form>
+        <ul className={styles.vizualization}>
+          {arrayChars?.map((item: string, index: number) => <li key={index}><Circle
+            state={modifiedElements.includes(index)
+              ? ElementStates.Modified
+              : changingElements.left === index || changingElements.right === index
+                ? ElementStates.Changing
+                : ElementStates.Default
+            }
+            letter={item}/></li>)}
+        </ul>
+      </div>
+    </SolutionLayout>
+  );
 };
