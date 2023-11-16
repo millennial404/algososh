08ea24bf0ac61ec0ad069dbd2767e00c5d354749
s.zaharchenko <s.zaharchenko@smrtc.ru>
2023-11-16 17:58:50 +0300

    feat: stack

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,69 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import styles from "./stack-page.module.css";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {useForm} from "../../hooks/useForm";
+import {Circle} from "../ui/circle/circle";
+import {ElementStates} from "../../types/element-states";
+import {DELAY_IN_MS} from "../../constants/delays";
 
 export const StackPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Стек">
+    const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
+    const {values, handleChange, setValues} = useForm({});
+    const [arrayChars, setArrayChars] = useState<(string | number)[]>([]);
+    const [lighting, setLighting] = useState<number | null>(null);
+    const add = async (e: React.FormEvent) => {
+        let newChars: (string | number)[] = [...arrayChars];
+        newChars.push(values.value)
+        setArrayChars([...newChars]);
+        const target = e.target as HTMLFormElement;
+        target.reset();
+        setValues({value: ''})
+        setLighting(newChars.length - 1)
+        await delay(DELAY_IN_MS);
+        setLighting(null)
 
-    </SolutionLayout>
-  );
+    }
+    const del = async () => {
+        let newChars: (string | number)[] = [...arrayChars];
+        setLighting(newChars.length - 1)
+        await delay(DELAY_IN_MS);
+        setLighting(null)
+        newChars.pop()
+        setArrayChars([...newChars]);
+        setValues({value: ''})
+    }
+    const clear = () => {
+        setArrayChars([]);
+    }
+    const handleSubmit = (evt: React.FormEvent) => {
+        evt.preventDefault();
+        if (values.value && values.value !== '') {
+            add(evt);
+        }
+    }
+    console.log(values)
+    return (
+        <SolutionLayout title="Стек">
+            <div className={styles.wrapper}>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <Input name={'value'} extraClass={'mr-6'} maxLength={4} isLimitText={true}
+                           onChange={handleChange}/>
+                    <Button extraClass={`ml-6`} text={'Добавить'} type={'submit'}/>
+                    <Button onClick={del} extraClass={`ml-6`} text={'Удалить'} type={'button'}/>
+                    <Button onClick={clear} extraClass={`ml-40`} text={'Очистить'} type={'button'}/>
+                </form>
+                <ul className={styles.vizualization}>
+                    {arrayChars?.map((item: string | number, index: number) => <li key={index}>
+                        <Circle
+                            head={index === arrayChars.length - 1 ? 'top' : null}
+                            state={lighting === index ? ElementStates.Changing : ElementStates.Default}
+                            letter={`${item}`}
+                            index={index}
+                        /></li>)}
+                </ul>
+            </div>
+        </SolutionLayout>
+    );
 };
